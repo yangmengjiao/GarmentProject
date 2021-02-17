34febe331cb0b5adff8bcbb71e28c88825490073
yangmengjiao <mengjiaoshun@gmail.com>
2021-02-17 12:20:51 -0600

    Initial Commit

---
+++ GarmentProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GarmentProject
+//
+//  Created by mengjiao on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
